bf6464b1512f7eadf9d7e2c38a2f794fbb62f563
Uni_verse <96498811+Yamada6612@users.noreply.github.com>
2023-04-16 16:13:06 -0300

    update Iniciar-servidor.sh

--- Iniciar-servidor.sh
+++ Iniciar-servidor.sh
@@ -2,27 +2,27 @@
 echo "Iniciando por favor aguarde"
 sleep 2
 clear
-echo "===============================================================================" |lolcat -F 0.2 -d 2
-echo "                           Menu 1m1-1J Minecraft server          " |lolcat -F 0.2 -d 2
-echo "===============================================================================" |lolcat -F 0.2 -d 2
+echo "===============================================================================" |lolcat -F 0.2 -d 1
+echo "                           Menu 1m1-1J Minecraft server          " |lolcat -F 0.2 -d 1
+echo "===============================================================================" |lolcat -F 0.2 -d 1
 #!/bin/bash
-echo -n "Escolha uma opção             "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "1. Iniciar                    "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "2. Parar                      "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "3. configuração               "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "4. Ultimo Log                 "; echo ",,,,.........,,,,,,,,........,,,," | lolcat -F 0.2 -d 2
-echo -n "5. Ver whitelist              "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 2
-echo -n "6. Ver players banidos        "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 2
-echo -n "7. IPs banidos                "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 2
-echo -n "8. Sair                       "; echo ",,,,,,,,,,,,.,,,,,,,.,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,,,,,         ,,,,,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,....         ....,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,     .......     ,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,    .,,,,,,,.    ,,,,,,,," | lolcat -F 0.2 -d 2
-echo -n "                              "; echo ",,,,,,,,    .,,,,,,,.    ,,,,,,,," | lolcat -F 0.2 -d 2
+echo -n "Escolha uma opção             "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "1. Iniciar                    "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "2. Parar                      "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "3. configuração               "; echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "4. Ultimo Log                 "; echo ",,,,.........,,,,,,,,........,,,," | lolcat -F 0.2 -d 1
+echo -n "5. Ver whitelist              "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 1
+echo -n "6. Ver players banidos        "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 1
+echo -n "7. IPs banidos                "; echo ",,,,        .,,,,,,,.        ,,,," | lolcat -F 0.2 -d 1
+echo -n "8. Sair                       "; echo ",,,,,,,,,,,,.,,,,,,,.,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,,,,,         ,,,,,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,....         ....,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,                 ,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,     .......     ,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,    .,,,,,,,.    ,,,,,,,," | lolcat -F 0.2 -d 1
+echo -n "                              "; echo ",,,,,,,,    .,,,,,,,.    ,,,,,,,," | lolcat -F 0.2 -d 1
 
 # Lê a escolha do usuário
 read escolha
