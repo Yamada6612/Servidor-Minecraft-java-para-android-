68b54ee2b3730640da687a8e6929507f9446e5ed
Uni_verse <96498811+Yamada6612@users.noreply.github.com>
2023-05-26 21:12:00 -0300

    update permissions setup-server-1.19.4.sh

--- setup-server-1.19.4.sh
+++ setup-server-1.19.4.sh
@@ -2,8 +2,8 @@ echo "Preparando para instalar os recursos para o servidor para o minecraft 1.19
 sleep 2
 apt update
 apt upgrade
-pkg install ruby
-gem install lolcat
+pkg install ruby -y
+gem install lolcat -y
 mkdir MC-1.19.4
 cd MC-1.19.4
 sleep 5
@@ -11,9 +11,10 @@ echo "==========================================================================
            echo "Ao iniciar o servidor use o comando "cd MC-1.19.4" para ir para a pasta com os arquivos e depois digite "sh Iniciar-servidor""
 echo "=============================================================================================================================
 sleep 10
-pkg install openjdk-17
+pkg install openjdk-17 -y
 wget https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar
 java -Xmx1024M -Xms1024M -jar server.jar
+sleep 2
 echo "eula=true" > eula.txt
 echo "Iniciando servidor"
 
